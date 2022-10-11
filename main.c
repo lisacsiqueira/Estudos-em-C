cdb0ad360bb8e66f74e94ee43b266640f5058640
Lisa Costa <112580152+lisacsiqueira@users.noreply.github.com>
2022-10-11 12:24:30 +0000

    atividade de introdução a linguagem C

--- main.c
+++ main.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
+#include <math.h>
+
+
 
 int main(void) {
-  printf("Hello World\n");
+/*
+// ex1
+  char nome[20];
+  printf("Digite seu nome: ");
+  scanf("%s", &nome);
+  
+  printf("\nola, %s!\nSeja bem vinda!",nome);
+*/
+  
 
+ /*
+  //ex02
+
+  float num1, num2, num3;
+  printf("Digite um numero: ");
+  scanf("%f", &num1);
+  printf("Digite um numero: ");
+  scanf("%f", &num2);
+  printf("Digite um numero: ");
+  scanf("%f", &num3);
+
+  float subtrair = num1 - num2;
+  float mult = subtrair * num3;
+
+  printf("%.2f", mult);*/
+
+/*
+//Desafio
+
+  int exp, potencia;
+  printf("Digite o expoente: ");
+  scanf("%d",&exp);
+
+  potencia = pow(2,exp);
+  printf("A potencia de 2 elevado a %d eh: %d", exp, potencia);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
   return 0;
 }
